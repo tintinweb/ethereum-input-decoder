20783ba08fdd9e41df5fbbc278bf30ed45e4b1f8
tintinweb <tintinweb@oststrom.com>
2018-06-12 21:34:50 +0200

    pump version 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="ethereum_input_decoder",
-    version="0.2",
+    version="0.2.1",
     packages=["ethereum_input_decoder"],
     author="tintinweb",
     author_email="tintinweb@oststrom.com",
@@ -18,7 +18,7 @@ setup(
     license="GPLv3",
     keywords=["ethereum", "blockchain", "input", "transaction", "decoder"],
     url="https://github.com/tintinweb/ethereum-input-decoder/",
-    download_url="https://github.com/tintinweb/ethereum-input-decoder/tarball/v0.2",
+    download_url="https://github.com/tintinweb/ethereum-input-decoder/tarball/v0.2.1",
     #python setup.py register -r https://testpypi.python.org/pypi
     long_description=read("README.rst") if os.path.isfile("README.rst") else read("README.md"),
     install_requires=["eth-abi", "requests"],
